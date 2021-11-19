22f42c9534d5d374af2a161a0004839e3e333d67
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2021-11-19 01:44:15 +0100

    Add greetings controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :greetings, only: [:index]
+    end
+  end
+
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
 end
